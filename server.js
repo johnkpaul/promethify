6f62e9f037c580d3aa5c20a0c8c691cc0123083b
John K. Paul <john@johnkpaul.com>
2013-12-22 15:20:18 -0500

    Fix problem with port number

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 'use strict';
 var http = require("http");
 var port = '8089';
+var findPackageJson = require('./findPackageJson');
+var pack = findPackageJson();
 
 module.exports = {
   start: function(bundles){
-    http.createServer(function(request, response) {
-      response.writeHead(200, {'Content-Type': 'application/javascript'});
-      response.end(bundles[request.url]);
+    pack.then(function(data){
+      var port = data.pack.promethify.port;
+      http.createServer(function(request, response) {
+        response.writeHead(200, {'Content-Type': 'application/javascript'});
+        response.end(bundles[request.url]);
 
-    }).listen(parseInt(port, 10));
+      }).listen(parseInt(port, 10));
+    
+    });
 
   }
 };
